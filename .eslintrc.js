62e1c9acf414a54dad17b29689f077a3deb0ea53
Joshua Young <joshua.r.young1@gmail.com>
2019-08-10 17:03:28 -0600

    init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  rules: {
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: ['error', 'never'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+}
